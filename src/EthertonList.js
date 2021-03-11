980dfe4ca693b7fc129f288ab0715897684c26eb
metherton <martinetherton63@gmail.com>
2021-03-12 00:27:58 +0100

    tidying up

--- src/EthertonList.js
+++ src/EthertonList.js
@@ -4,16 +4,7 @@ import { LitElement, html } from 'lit-element';
 export class EthertonList extends LitElement {
   static get styles() {
     return css`
-      :host {
-        min-height: 100vh;
-        display: block;
-        align-items: center;
-        font-size: calc(10px + 2vmin);
-        color: #1a2b42;
-        max-width: 960px;
-        margin: 0 auto;
-        text-align: center;
-      }
+
     `;
   }
 
--- src/EthertonListItem.js
+++ src/EthertonListItem.js
@@ -5,16 +5,7 @@ export class EthertonListItem extends LitElement {
   static get styles() {
     return css`
       :host {
-        min-height: 10vh;
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        justify-content: flex-start;
-        font-size: calc(10px + 2vmin);
-        color: #1a2b42;
-        max-width: 960px;
-        margin: 0 auto;
-        text-align: center;
+        display: block;
       }
     `;
   }
