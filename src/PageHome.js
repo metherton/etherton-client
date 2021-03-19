12a312048d15de77a8f5e4aa70c3fa9cd12a122e
metherton <martinetherton63@gmail.com>
2021-03-19 09:42:15 +0100

    renamed

--- src/PageHome.js
+++ src/PageHome.js
@@ -25,8 +25,8 @@ export class PageHome extends LitElement {
   async getLosers() {
     let body = 'xCsrfToken=' + Login.xCsrfToken || "";
     //fetch('https://www.martinetherton.com:8443/secured')
-    const response = await fetch('https://localhost:8443/losers', {
-    //const response = await fetch('https://www.martinetherton.com:8443/losers', {
+    //const response = await fetch('https://localhost:8443/losers', {
+    const response = await fetch('https://www.martinetherton.com:8443/losers', {
     //const response = await fetch('http://localhost:8080/losers', {
       method: 'POST',
       credentials: 'include',
@@ -55,8 +55,8 @@ export class PageHome extends LitElement {
 
   logout() {
     let body = 'xCsrfToken=' + Login.xCsrfToken || "";
-    fetch('https://localhost:8443/logout', {
-    //fetch('https://www.martinetherton.com:8443/logout', {
+    //fetch('https://localhost:8443/logout', {
+    fetch('https://www.martinetherton.com:8443/logout', {
     //const response = await fetch('http://localhost:8080/losers', {
       method: 'POST',
       credentials: 'include',
