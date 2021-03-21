4f50c6a52defa85550d9150836f7311910b31edd
metherton <martinetherton63@gmail.com>
2021-03-21 23:59:10 +0100

    removed https

--- src/PageHome.js
+++ src/PageHome.js
@@ -22,12 +22,30 @@ export class PageHome extends LitElement {
     super.connectedCallback();
   }
 
+  handleLosers() {
+    this.getLosers().then(response => {
+      if (response.status == 401) {
+        this.dispatchEvent(new CustomEvent('login', {bubbles: true, detail: 'login'}));
+        return;
+      }
+      return response.json();
+    })
+    .then(data => {
+      this.losers = data;
+    })
+    .catch(err => {
+      console.log(err);
+    });
+  }
+
   async getLosers() {
     let body = 'xCsrfToken=' + Login.xCsrfToken || "";
     //fetch('https://www.martinetherton.com:8443/secured')
-    //const response = await fetch('https://localhost:8443/losers', {
-    const response = await fetch('https://www.martinetherton.com:8443/losers', {
+   // const response = await fetch('https://localhost:8443/losers', {
+    //const response = await fetch('https://www.martinetherton.com:8443/losers', {
     //const response = await fetch('http://localhost:8080/losers', {
+    const response = await fetch('http://www.martinetherton.com:8080/losers', {
+
       method: 'POST',
       credentials: 'include',
       mode: 'cors',
@@ -55,8 +73,10 @@ export class PageHome extends LitElement {
 
   logout() {
     let body = 'xCsrfToken=' + Login.xCsrfToken || "";
+    //fetch('http://localhost:8080/logout', {
+    fetch('http://www.martinetherton.com:8080/logout', {
     //fetch('https://localhost:8443/logout', {
-    fetch('https://www.martinetherton.com:8443/logout', {
+    //fetch('https://www.martinetherton.com:8443/logout', {
     //const response = await fetch('http://localhost:8080/losers', {
       method: 'POST',
       credentials: 'include',
@@ -73,29 +93,19 @@ export class PageHome extends LitElement {
     this.dispatchEvent(new CustomEvent('logout', {bubbles: true, detail: 'home'}));
   }
 
-  handleLosers() {
-    this.getLosers().then(response => {
-      if (response.status == 401) {
-        this.dispatchEvent(new CustomEvent('login', {bubbles: true, detail: 'login'}));
-        return;
-      }
-      return response.json();
-    })
-    .then(data => {
-      this.losers = data;
-    })
-    .catch(err => {
-      console.log(err);
-    });
-  }
-
   render() {
     return html`
         ${this.losers?
         html`
         <button @click="${this.logout}">Logout</button><button @click="${this.handleLosers}">Get Losers</button>
         <etherton-list>
-          ${this.losers.map(loser => html`<etherton-list-item>${loser.companyName}</etherton-list-item>`)}
+          ${this.losers.map(loser => html`
+          <etherton-list-item>${loser.companyName}</etherton-list-item>
+          <etherton-list-item>${loser.changes}</etherton-list-item>
+          <etherton-list-item>${loser.changesPercentage}</etherton-list-item>
+          <etherton-list-item>${loser.price}</etherton-list-item>
+          <etherton-list-item>${loser.ticker}</etherton-list-item>
+          `)}
         </etherton-list>`:
          html`no companies&nbsp;<button @click="${this.handleLosers}">Get Losers</button>`}
     `;
