0d3aa80122238e287236786cf77f1c587eb6b7ba
metherton <martinetherton63@gmail.com>
2021-07-13 21:32:20 +0200

    Added PageSearch

--- src/PageSearch.js
+++ src/PageSearch.js
@@ -1,8 +1,8 @@
 import { LitElement, html, css } from 'lit-element';
 import { PersonList } from './PersonList.js';
-import { PageSearch } from './PageSearch.js';
+import { PersonItem } from './PersonItem.js';
 
-export class PageHome extends LitElement {
+export class PageSearch extends LitElement {
 
   static get properties() {
     return {
@@ -70,10 +70,36 @@ export class PageHome extends LitElement {
     return html`
       <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
       <link rel="stylesheet" href="https://www.w3schools.com/lib/w3-theme-indigo.css">
-      <page-search></page-search>
+      <div class="w3-container w3-margin">
+
+        <div class="w3-container w3-theme-l4">
+          <h2>Search</h2>
+        </div>
+        <form class="w3-container">
+          <p>
+            <label>First Name</label>
+            <input class="w3-input" type="text">
+          </p>
+          <p>
+            <label>Last Name</label>
+            <input class="w3-input" type="text">
+          </p>
+          <p>
+            <label>Place your ancestor might have lived</label>
+            <input class="w3-input" type="text">
+          </p>
+          <p>
+            <label>Birth Year</label>
+            <input class="w3-input" type="text">
+          </p>
+          <p>
+            <input class="w3-btn w3-block w3-theme-d1" type="button" value="Search">
+          </p>
+        </form>
+      </div>
     `;
   }
 
 }
 
-customElements.define('page-home', PageHome);
+customElements.define('page-search', PageSearch);
