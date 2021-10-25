6b01daa15dd20d8e0dd5c601d4e6ae8291624555
metherton <martinetherton63@gmail.com>
2021-10-25 23:10:08 +0200

    make font smaller

--- src/PageHome.js
+++ src/PageHome.js
@@ -99,12 +99,50 @@ export class PageHome extends LitElement {
         /* Remove margins from "page content" on small screens */
         @media only screen and (max-width: 600px) {#main {margin-left: 0}}
       </style>
+
+      <nav class="w3-sidebar w3-bar-block w3-small w3-hide-small w3-center" style="background: white">
+        <!-- Avatar image in top left corner -->
+        <img src="/images/mart.png" style="width:100%">
+        <a href="#" class="w3-bar-item w3-button w3-padding-large w3-black">
+          <i class="fa fa-home w3-xxlarge"></i>
+          <p>HOME</p>
+        </a>
+        <a href="#about" class="w3-bar-item w3-button w3-padding-large w3-hover-black">
+          <i class="fa fa-user w3-xxlarge"></i>
+          <p>ABOUT</p>
+        </a>
+        <a href="#ons" class="w3-bar-item w3-button w3-padding-large w3-black">
+          <i class="fa fa-tree w3-xxlarge"></i>
+          <p>ONS</p>
+        </a>
+        <a href="#photos" class="w3-bar-item w3-button w3-padding-large w3-hover-black">
+          <i class="fa fa-eye w3-xxlarge"></i>
+          <p>PHOTOS</p>
+        </a>
+        <a href="#contact" class="w3-bar-item w3-button w3-padding-large w3-hover-black">
+          <i class="fa fa-envelope w3-xxlarge"></i>
+          <p>CONTACT</p>
+        </a>
+      </nav>
+
+      <!-- Navbar on small screens (Hidden on medium and large screens) -->
+      <div class="w3-top w3-hide-large w3-hide-medium" id="myNavbar" style="background: white">
+        <div class="w3-bar w3-opacity w3-hover-opacity-off w3-center w3-small">
+          <a href="#" class="w3-bar-item w3-button" style="width:20% !important">HOME</a>
+          <a href="#about" class="w3-bar-item w3-button" style="width:20% !important">ABOUT</a>
+          <a href="#ons" class="w3-bar-item w3-button" style="width:20% !important">ONS</a>
+          <a href="#photos" class="w3-bar-item w3-button" style="width:20% !important">PHOTOS</a>
+          <a href="#contact" class="w3-bar-item w3-button" style="width:20% !important">CONTACT</a>
+        </div>
+      </div>
+
+
       <!-- Page Content -->
       <div class="w3-padding-large" id="main">
         <!-- Header/Home -->
         <header class="w3-container w3-padding-32 w3-center w3-black" id="home">
           <h1 class="w3-jumbo"><span class="w3-hide-small">Martin Etherton</span></h1>
-          <h3 class="w3-jumbo"><span class="w3-hide-small">Full stack developer</span></h3>
+          <h4 class="w3-xxlarge"><span class="w3-hide-small">Full stack developer</span></h3>
           <img src="/images/mart.png" alt="me" class="w3-image" width="992" height="1108">
         </header>
       </div>
