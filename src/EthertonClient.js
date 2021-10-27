4b996bb2d5dd6e09cec40162ebf1ca54be80d14b
metherton <martinetherton63@gmail.com>
2021-10-27 21:01:54 +0200

    use responsive styles

--- src/EthertonClient.js
+++ src/EthertonClient.js
@@ -103,41 +103,7 @@ export class EthertonClient extends LitElement {
         @media only screen and (max-width: 600px) {#main {margin-left: 0}}
       </style>
 
-      <nav class="w3-sidebar w3-bar-block w3-small w3-hide-small w3-center" style="background: white">
-        <!-- Avatar image in top left corner -->
-        <img src="/images/mart.png" style="width:100%">
-        <a href="#" @click=${this.navigatePage} class="w3-bar-item w3-button w3-padding-large w3-black">
-          <i class="fa fa-home w3-xxlarge"></i>
-          <p>HOME</p>
-        </a>
-        <a href="#about" @click=${this.navigatePage} class="w3-bar-item w3-button w3-padding-large w3-hover-black">
-          <i class="fa fa-user w3-xxlarge"></i>
-          <p>ABOUT</p>
-        </a>
-        <a href="#ons" @click=${this.navigatePage} class="w3-bar-item w3-button w3-padding-large w3-black">
-          <i class="fa fa-tree w3-xxlarge"></i>
-          <p>ONS</p>
-        </a>
-        <a href="#photos" @click=${this.navigatePage} class="w3-bar-item w3-button w3-padding-large w3-hover-black">
-          <i class="fa fa-eye w3-xxlarge"></i>
-          <p>PHOTOS</p>
-        </a>
-        <a href="#contact" @click=${this.navigatePage} class="w3-bar-item w3-button w3-padding-large w3-hover-black">
-          <i class="fa fa-envelope w3-xxlarge"></i>
-          <p>CONTACT</p>
-        </a>
-      </nav>
 
-      <!-- Navbar on small screens (Hidden on medium and large screens) -->
-      <div class="w3-top w3-hide-large w3-hide-medium" id="myNavbar" style="background: white">
-        <div class="w3-bar w3-opacity w3-hover-opacity-off w3-center w3-small">
-          <a href="#" @click=${this.navigatePage} class="w3-bar-item w3-button" style="width:20% !important">HOME</a>
-          <a href="#about" @click=${this.navigatePage} class="w3-bar-item w3-button" style="width:20% !important">ABOUT</a>
-          <a href="#ons" @click=${this.navigatePage} class="w3-bar-item w3-button" style="width:20% !important">ONS</a>
-          <a href="#photos" @click=${this.navigatePage} class="w3-bar-item w3-button" style="width:20% !important">PHOTOS</a>
-          <a href="#contact" @click=${this.navigatePage} class="w3-bar-item w3-button" style="width:20% !important">CONTACT</a>
-        </div>
-      </div>
 
       <main>
         ${this._renderPage()}
